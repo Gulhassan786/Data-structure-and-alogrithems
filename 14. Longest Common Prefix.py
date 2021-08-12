3456f3dadd7a4fb24611b23d41b46c65d27dedfa
gulhassanh49@gmail.com <gulhassanh49@gmail.com>
2021-08-12 21:42:28 +0500

    Added problem NO: 14 from LeetCode

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,17 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+# If there is no common prefix, return an empty string "".
+
+
+    def longestCommonPrefix(strs: List[str]) -> str:
+
+        common = []
+        for l in strs[0]:
+            if all(s.startswith("".join(common)+l) for s in strs):
+                common.append(l)
+            else:
+                break
+        return "".join(common)
+
+
+print(longestCommonPrefix( ["flower","flow","flight"]))
